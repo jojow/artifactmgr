96d51e4805a02e276172acd0fa9a8bd87679aaec
Johannes <jowettinger@gmail.com>
2014-08-01 21:18:25 +0200

    nvm calls fixed

--- build-on-ubuntu.sh
+++ build-on-ubuntu.sh
@@ -13,13 +13,14 @@ fi
 apt-get -y update && apt-get -y install git curl python-software-properties python g++ make
 
 curl https://raw.githubusercontent.com/creationix/nvm/v0.12.2/install.sh | bash
-source ~/.nvm/nvm.sh
-nvm install 0.10
-nvm use 0.10
-nvm alias default 0.10
+~/.nvm/nvm.sh install 0.10
+~/.nvm/nvm.sh use 0.10
+~/.nvm/nvm.sh alias default 0.10
 
 rm -rf $ARTIFACT_MANAGER_DIR
 git clone https://github.com/jojow/artifactmgr.git $ARTIFACT_MANAGER_DIR
+
+chmod a+x $ARTIFACT_MANAGER_DIR/run.sh
 
 #
 # Run artifact manager:
