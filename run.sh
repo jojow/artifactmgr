fffa4ca05ec737712ea130f7a8c59923cfe06272
Johannes <jojow@users.noreply.github.com>
2015-08-21 10:52:07 +0200

    forever: append logs

--- run.sh
+++ run.sh
@@ -17,5 +17,5 @@ source $NVM_DIR/nvm.sh
 
 cd $ARTIFACT_MANAGER_DIR
 
-DEBUG="artifactmgr:*" forever -l forever.log -o out.log -e err.log server.js
+DEBUG="artifactmgr:*" forever -a -l forever.log -o out.log -e err.log server.js
 #DEBUG="artifactmgr:*" npm start 2>&1 | tee -a $ARTIFACT_MANAGER_DIR/artifactmgr.log
